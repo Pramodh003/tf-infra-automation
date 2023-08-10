32c60fbcc578c4697b14cb359955d727fbd207ab
pramodh-sirpi <pramodh@sirpi.io>
2023-08-10 21:02:26 +0530

    fixed port isse

--- terraform/main_app.tf
+++ terraform/main_app.tf
@@ -69,7 +69,7 @@ resource "aws_ecs_service" "app-ecs-service" {
   load_balancer {
     target_group_arn = aws_lb_target_group.app-tg
     container_name = "app"
-    port = 80
+    container_port = 80
   }
 }
 
